d3a852c8dfcd257279df442d1ae73e85a976a90c
GurayCoskun <gry_cskn.98@hotmail.com>
2021-12-20 17:50:35 +0300

    safe area view

--- Marvel/App.js
+++ Marvel/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {  Image,SafeAreaView} from 'react-native';
 import Router from './src/navigation/router'
 export default function App() {
   return (
     <SafeAreaProvider>
+      <SafeAreaView style={{flex:1}}>
           <Router/>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
